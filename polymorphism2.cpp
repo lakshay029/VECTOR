c47f970e4bb2ea58c38fa32efe2bacdb7d81a652
lakshay029 <126890120+lakshay029@users.noreply.github.com>
2023-05-17 22:30:51 +0530

    Add files via upload

---
+++ polymorphism2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class complex {
+    public:
+    int real , img;
+    public:
+    complex(int r=0,int i=0){
+        real =r;
+        img=i;
+    }
+    complex operator+(complex const& obj){
+        complex res;
+        res.real=real+obj.real;
+        res.img=img+obj.img;
+    }
+    void print(){cout<<real<<"+i"<<img<<endl;}
+};
+int main(){
+    complex c1(10,5) ,c2(3,7);
+    complex c3=c1+c2; 
+    c3.print();
+}
